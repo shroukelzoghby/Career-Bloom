274f9b37b82e57dde6ec1e38fbc6bf3477d721f5
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-27 08:06:47 +0300

    UPDATE: Update differentiate between featured and recent jobs and update views

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -4,14 +4,14 @@
     <x-employer-logo></x-employer-logo>
     </div>
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60.000</p>
+        <a href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{$job->title}}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->schedule}} - {{$job->salary}}</p>
     </div>
 
     <div>
         @foreach($job->tags as $tag)
-            <x-tag :$tag >Backend</x-tag>
+            <x-tag :$tag />
         @endforeach
     </div>
 </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,14 +1,14 @@
 @props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{$job->employer->name}}</div>
     <div class="py-8 ">
-        <h3 class="group-hover:text-blue-800 transition-colors duration-300 text-xl font-bold">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60.000</p>
+        <h3 class="group-hover:text-blue-800 transition-colors duration-300 text-xl font-bold">{{$job->title}}</h3>
+        <p class="text-sm mt-4">{{$job->schedule}} - {{$job->salary}}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
             @foreach($job->tags as $tag)
-                <x-tag :$tag size="small">Backend</x-tag>
+                <x-tag :$tag size="small"/>
             @endforeach
 
 
