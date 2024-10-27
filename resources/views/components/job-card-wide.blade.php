ece1d80d7bc7030b35a80594e191af916fcca8ad
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-27 07:49:13 +0300

    UPDATE: Update seeders and data in views

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,3 +1,4 @@
+@props(['job'])
 <x-panel class="flex gap-x-6">
     <div>
     <x-employer-logo></x-employer-logo>
@@ -9,8 +10,8 @@
     </div>
 
     <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag >Backend</x-tag>
+        @endforeach
     </div>
 </x-panel>
