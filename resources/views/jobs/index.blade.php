b4cf430df0ddafd81758be36847eb1ae707b60fe
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-28 06:30:02 +0300

    FEAT: Add search system functionality

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,7 +3,7 @@
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
             <x-forms.form action="/search" class="mt-6">
-                <x-forms.input :lable="false" name="q" placeholder="Web Development..."></x-forms.input>
+                <x-forms.input :label="false" name="q" placeholder="Web Development..."></x-forms.input>
             </x-forms.form>
         </section>
         <section class="pt-10">
